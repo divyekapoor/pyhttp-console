ac60d135546c5b16311b14911bb99608c0a24716
Divye Kapoor <divyekapoor@gmail.com>
2010-12-29 18:01:00 +0530

    Add License Text to the code

--- http-console.py
+++ http-console.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
+#
+#          Copyright Divye Kapoor 2010
+# Distributed under the Boost Software License, Version 1.0.
+#    (See accompanying file LICENSE_1_0.txt or copy at
+#          http://www.boost.org/LICENSE_1_0.txt)
+#
 import optparse, sys, urllib2
 import simplejson as json
 
